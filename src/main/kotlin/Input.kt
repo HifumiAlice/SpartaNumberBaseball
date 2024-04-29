2ba0720d6be5b1c173c869bf42c687ceadb20a43
Alice <hifumialice@gmail.com>
2024-04-29 14:54:20 +0900

    숫자입력 받기

--- src/main/kotlin/Input.kt
+++ src/main/kotlin/Input.kt
@@ -23,6 +23,57 @@ fun selectMode() : Int {
     }
 }
 
-fun inputAnswer() {
+fun inputAnswer() : String{
+//    var
+    var number : Int
+    var input : String
+
+
+    while (true) {
+        println("세 자리 수를 입력해 주세요 \nex) 123 / 714")
+        print("입력 : ")
+        input = scanner.next()
+
+        if (input.length != 3) {
+            println("=========================")
+            println("세자리 수를 입력해 주세요")
+            println("=========================")
+            continue
+        } else if (input[0] == '0') {
+            println("=========================")
+            println("첫 자리에는 0이 올 수 없습니다.")
+            println("다시 입력해주세요")
+            println("=========================")
+            continue
+        } else if (input[0] == input [1] || input[1] == input[2] || input[2] == input[0]) {
+            println("=========================")
+            println("중복된 숫자가 올 수 없습니다.")
+            println("=========================")
+            continue
+        }
+
+        try {
+            input.toInt()
+            return  input
+        } catch (e: NumberFormatException) {
+            println("=========================")
+            println("숫자를 입력해 주세요")
+            println("=========================")
+            continue
+        }
+
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
