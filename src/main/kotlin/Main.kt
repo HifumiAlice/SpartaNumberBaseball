113f89c63a188b18c0c26b9df369c0848dbafcac
Alice <hifumialice@gmail.com>
2024-05-02 11:52:55 +0900

    Run

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,7 @@
 package baseball
 
-import java.util.*
 
 fun main() {
-
-    val gameScore : MutableList<Int> = mutableListOf()
-
-    while (true) {
-        when (Input.selectMode()) {
-            1 -> {
-                TextPrint.gameStart()
-                gameScore.add(PlayGame().run())
-                parameterPrint.equalAnswer(gameScore[gameScore.size - 1])
-            }
-            2 -> parameterPrint.recordPlay(gameScore)
-            3 -> {
-                TextPrint.endGame()
-                break
-            }
-        }
-    }
-
-
-
+    GameManager().selectGame()
 }
 
