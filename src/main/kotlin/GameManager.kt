d56a5c517583b7dc8a5fc6970cd9f89ccc6e9cd8
Alice <hifumialice@gmail.com>
2024-05-02 12:02:49 +0900

    Select game is NumberBaseball

--- src/main/kotlin/GameManager.kt
+++ src/main/kotlin/GameManager.kt
@@ -2,23 +2,10 @@ package baseball
 
 class GameManager {
 
-    val gameList : MutableList<Game> = mutableListOf(NumberBaseball())
-    val info = InformationPrint()
+    private val game = NumberBaseball()
+    private val info = InformationPrint()
 
     fun selectGame() {
-        if (gameList.size == 0) {
-            info.info("실행할 수 있는 게임이 없습니다.")
-            return
-        } else {
-            info.info("게임목록\n")
-            for (i in gameList.indices) {
-                println("${i+1}번 게임 : ${gameList[i].name}")
-            }
-            info.select("시작할 게임 번호를 입력해주세요\n입력 : ")
-            GameRunner().run(gameList[Person().selectGame(gameList.size)-1])
+            GameRunner().run(game)
         }
     }
-
-
-
-}
